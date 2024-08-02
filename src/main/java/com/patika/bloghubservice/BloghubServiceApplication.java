7cd3d0d4b12e34d3cac9555bdd82d37045d1020b
cemdrman <cemdrman@gmail.com>
2024-08-03 02:08:29 +0300

    rabbit connection

--- src/main/java/com/patika/bloghubservice/BloghubServiceApplication.java
+++ src/main/java/com/patika/bloghubservice/BloghubServiceApplication.java
@@ -2,10 +2,12 @@ package com.patika.bloghubservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class BloghubServiceApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/patika/bloghubservice/service/UserService.java
+++ src/main/java/com/patika/bloghubservice/service/UserService.java
@@ -1,17 +1,18 @@
 package com.patika.bloghubservice.service;
 
-import com.patika.bloghubservice.client.email.service.EmailClientService;
-import com.patika.bloghubservice.client.email.dto.request.EmailCreateRequest;
 import com.patika.bloghubservice.client.email.dto.request.enums.EmailTemplate;
+import com.patika.bloghubservice.client.email.service.EmailClientService;
 import com.patika.bloghubservice.client.payment.service.PaymentClientService;
 import com.patika.bloghubservice.client.payment.service.dto.request.PaymentRequest;
 import com.patika.bloghubservice.converter.UserConverter;
+import com.patika.bloghubservice.dto.producer.SendEmailMessage;
 import com.patika.bloghubservice.dto.request.UserSaveRequest;
 import com.patika.bloghubservice.dto.response.UserResponse;
 import com.patika.bloghubservice.exception.BlogHubException;
 import com.patika.bloghubservice.exception.ExceptionMessages;
 import com.patika.bloghubservice.model.User;
 import com.patika.bloghubservice.model.enums.StatusType;
+import com.patika.bloghubservice.producer.RabbitMqProducer;
 import com.patika.bloghubservice.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -32,6 +33,7 @@ public class UserService {
     private final UserRepository userRepository;
     private final EmailClientService emailClientService;
     private final PaymentClientService paymentClientService;
+    private final RabbitMqProducer rabbitMqProducer;
 
     public UserResponse saveUser(UserSaveRequest request) {
 
@@ -48,7 +50,10 @@ public class UserService {
             User savedUser = new User(request.getEmail(), request.getPassword()); // ödev password' hash'le
 
             userRepository.save(savedUser);
-            emailClientService.sendEmail(new EmailCreateRequest(request.getEmail(), EmailTemplate.CREATE_USER_TEMPLATE));
+
+            rabbitMqProducer.sendEmail(new SendEmailMessage(request.getEmail(), EmailTemplate.CREATE_USER_TEMPLATE));
+
+            //  emailClientService.sendEmail(new EmailCreateRequest(request.getEmail(), EmailTemplate.CREATE_USER_TEMPLATE));
 
             return UserConverter.toResponse(savedUser);
         }
@@ -63,6 +68,8 @@ public class UserService {
     }
 
     public List<UserResponse> getAllUsers() {
+
+        log.info("get all users methodu çağrıldı" );
         List<User> users = userRepository.findAll();
         return UserConverter.toResponse(users);
     }
